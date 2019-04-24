c9b8cbea8d254cfac98e3540e740c96932c6fe12
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-04-24 12:31:46 +0300

    Add boilerplate code.

---
+++ encryption schemes/utils/numeric_convertion.py
@@ -0,0 +1,14 @@
+"""
+    Module: Numeric Convention.
+    Author: Kelyn Paul Njeri.
+    Date: 24th April 2019
+"""
+class NumericOperation():
+    def text_to_numerics(self, plain_text=None):
+        pass
+    
+    def numerics_to_text(self, numerics=None):
+        pass
+
+    def check_co_prime(self, a=None, b=None):
+        pass
