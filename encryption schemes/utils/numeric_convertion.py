117dadbabca6b1e49959c8b801cb54d7fe9700e3
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-04-24 14:28:08 +0300

    Add code to convert text to their respective numerical values.

--- encryption schemes/utils/numeric_convertion.py
+++ encryption schemes/utils/numeric_convertion.py
@@ -3,10 +3,19 @@
     Author: Kelyn Paul Njeri.
     Date: 24th April 2019
 """
+
+
 class NumericOperation():
     def text_to_numerics(self, plain_text=None):
-        pass
-    
+        for i, letter in enumerate(plain_text):
+            if i < len(plain_text):
+                if i is 0:
+                    numeral = ord(letter) - 65
+                else:
+                    numeral = ord(letter) - 97
+
+                return numeral
+
     def numerics_to_text(self, numerics=None):
         pass
 
