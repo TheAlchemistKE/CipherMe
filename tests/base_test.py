1d587110b4e1e18373bbd221052bb474436847e6
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-04-25 17:12:37 +0300

    Refactor project structure.

--- tests/base_test.py
+++ tests/base_test.py
@@ -3,5 +3,6 @@ import unittest
 class TestBaseCase(unittest.TestCase):
     def setUp(self):
         pass
+
     def tearDown(self):
         pass
---
+++ tests/test_affine_cipher.py
@@ -0,0 +1,14 @@
+from .base_test import TestBaseCase as base
+
+class TestAffineCipher(base):
+    def setUp(self):
+        base.setUp(self)
+
+    def test_plain_text_to_cipher_text(self):
+        pass
+
+    def test_cipher_text_to_plain_text(self):
+        pass
+
+    def tearDown(self):
+        base.tearDown(self)
