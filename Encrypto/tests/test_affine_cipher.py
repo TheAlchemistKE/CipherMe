7bb3a0b2d8be244a7758c5bd9084a65ff9383a6c
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-04-30 02:08:08 +0300

    Refactor folder structure and code of Encrypto.

--- Encrypto/tests/test_affine_cipher.py
+++ Encrypto/tests/test_affine_cipher.py
@@ -1,11 +1,13 @@
 from .base_test import TestBaseCase as base
 
+
 class TestAffineCipher(base):
     def setUp(self):
         base.setUp(self)
 
     def test_plain_text_to_cipher_text(self):
-        pass
+        self.assertEqual(self.affine.encrypt_text(
+            plain_text='Kelyn', a=7, b=15), 'Hrobc')
 
     def test_cipher_text_to_plain_text(self):
         pass
