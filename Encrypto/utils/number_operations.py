e96a9472808a71d5db8488700e050046d66d0ae4
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-05-01 13:23:22 +0300

    Add function to calculate multiplicative inverse.

--- Encrypto/utils/number_operations.py
+++ Encrypto/utils/number_operations.py
@@ -27,6 +27,36 @@ class NumberOperations():
                 self.cipher_text.append(letter)
         return ''.join(self.cipher_text)
 
+    def multiplicative_inverse(self, a=None, m=None):
+        """Modular Inverse"""
+        m0 = m
+        y = 0
+        x = 1
+
+        if (m == 1):
+            return 0
+
+        while (a > 1):
+            # q is quotient
+            q = a // m
+
+            t = m
+
+            # m is remainder now, process
+            # same as Euclid's algo
+            m = a % m
+            a = t
+            t = y
+
+            # Update x and y
+            y = x - q * y
+            x = t
+
+        # Make x positive
+        if (x < 0):
+            x = x + m0
+
+        return x
 
 def __gcd(a=None, b=None):
     # Everything divides 0
